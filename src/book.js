21d8fc13306b5b4a90cb08ae7c441c7c17f46529
chad-dickerson <chad.alan.dickerson@gmail.com>
2020-05-10 20:52:18 -0600

    Added book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,39 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name: name, age: age, pronouns: pronouns}
+};
+
+
+function saveReview(phrase, reviews) {
+if (reviews.includes(phrase) === false) {
+   reviews.push(phrase)
+}
+};
+
+function calculatePageCount(bookTitle) {
+return bookTitle.length * 20
 }
 
+function writeBook(bookTitle, bookCharacter, genre) {
+var book = {
+  title: bookTitle,
+  mainCharacter: bookCharacter,
+  genre: genre,
+  }
+  book.pageCount = calculatePageCount(bookTitle)
+  return book;
+}
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75
+}
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
